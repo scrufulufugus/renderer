1814a955ae72a16c5babed80ebff637145a54b22
scrufulufugus <smonson@irbash.net>
2021-08-26 15:07:16 -0700

    Support item files with mako

--- renderer.py
+++ renderer.py
@@ -6,29 +6,44 @@ import sys
 import argparse
 import csv
 from mako.template import Template
-from mako.runtime import Context
 
 def main(t_file, m_file, i_files, o_path, id_col="id", title_col="title"):
     template = Template(filename=t_file, format_exceptions=True)
 
-    outfiles = {}
+    context_tree = {}
 
     with open(m_file, 'r') as f:
         reader = csv.DictReader(f, delimiter=',', quotechar='"')
-        #main_data_vars = reader.fieldnames
         for row in reader:
-            outfiles[row[id_col]] = [row[title_col], template.render_unicode(**row)]
+            context_tree[row[id_col]] = row
 
-        for file_id, file_array in outfiles.items():
-            with open(o_path + '/' + file_array[0].replace('/', '_') + '.html', 'w') as f:
-                f.write(file_array[1])
+    for item, filename in i_files.items():
+        with open(filename) as f:
+            reader = csv.DictReader(f, delimiter=',', quotechar='"')
+            for row in reader:
+                if row[id_col] in context_tree.keys():
+                    if item in context_tree[row[id_col]].keys():
+                        context_tree[row[id_col]][item].append(row)
+                    else:
+                        context_tree[row[id_col]][item] = [row]
+
+    out_files = {}
+    for row in context_tree.values():
+        out_files[row[title_col]] = template.render_unicode(**row)
+
+    for filename, contents in out_files.items():
+        with open(o_path + '/' + filename.replace('/', '_') + '.html', 'w') as f:
+            f.write(contents)
+
+def parse_items(i_files, context):
+    pass
 
 if __name__ == __name__:
     parser = argparse.ArgumentParser(description='Fill template based off spreadsheets.')
     parser.add_argument('-t', '--template', dest='template', required=True)
     parser.add_argument('-m', '--main-data', dest='main', required=True)
     parser.add_argument('-o', '--out-path', dest='out', required=True)
-    parser.add_argument('-i', '--item-data', dest='items', nargs='*', default=[])
+    parser.add_argument('-i', '--item-data', dest='items', nargs=2, action='append', default=[])
     args = parser.parse_args()
 
     if not path.isfile(args.template):
@@ -40,11 +55,15 @@ if __name__ == __name__:
     if not path.isdir(args.out):
         print("Invalid out path: {}".format(args.out))
         sys.exit(2)
-    for i, item in enumerate(args.items):
-        if not path.isfile(item):
-            print("Invalid item: {}".format(item))
+    items = {}
+    for item in args.items:
+        if not path.isfile(item[1]):
+            print("Invalid item path: {}".format(item[1]))
             sys.exit(2)
-        args.items[i] = path.abspath(item)
+        if item[0] in items.keys():
+            print("Duplicate item: {}".format(item[0]))
+            sys.exit(2)
+        items[item[0]] = path.abspath(item[1])
 
     main(path.abspath(args.template), path.abspath(args.main),
-         args.items, path.abspath(args.out))
+         items, path.abspath(args.out))
