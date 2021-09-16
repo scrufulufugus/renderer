d23354b63a649e81e1c16af751cd05b2c4d46126
scrufulufugus <smonson@irbash.net>
2021-09-16 10:37:46 -0700

    Merge license

--- renderer.py
+++ renderer.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+"""
+Copyright (C) 2021 Samuel Monson <smonson at irbash dot net>
+
+This file is part of renderer.
+
+    renderer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    renderer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with renderer.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import os.path as path
 import sys
